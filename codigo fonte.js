f10dbb1b4bc1a712f28705030c0952b8c6e1b342
Danielgroth1 <daniielgroth@gmail.com>
2026-03-12 21:11:49 -0300

    Update codigo fonte.js

--- codigo fonte.js
+++ codigo fonte.js
@@ -3,3 +3,4 @@ Segurança:
 As senhas precisam conter carácteres especiais.
 Necessário autenticador para acessar o super usuário do sistema.
 O sistema deve ser atualizado constantemente para evitar falhas de segurança.
+  Editado, OK!
