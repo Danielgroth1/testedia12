3665402b9e342344d055fae2058f9c205119efe2
Daniel <daniielgroth@gmail.com>
2026-03-19 21:53:04 -0300

    cadastro cliente java

---
+++ cadastro_cliente.js
@@ -0,0 +1,38 @@
+// Variável LGC para mensagens
+const LGC = "Sistema de Cadastro de Cliente";
+
+// Dado do cliente
+const cliente = {
+  nome: "",
+  email: "",
+  telefone: ""
+};
+
+// Função 1: Exibir título do sistema
+function exibirTitulo() {
+  console.log("=".repeat(40));
+  console.log(LGC);
+  console.log("=".repeat(40));
+}
+
+// Função 2: Cadastrar cliente
+function cadastrarCliente(nome, email, telefone) {
+  cliente.nome = nome;
+  cliente.email = email;
+  cliente.telefone = telefone;
+  console.log(LGC + " - Cliente cadastrado com sucesso!");
+  console.log("Nome: " + cliente.nome);
+}
+
+// Função 3: Listar dados do cliente
+function listarCliente() {
+  console.log(LGC + " - Dados do Cliente:");
+  console.log("Nome: " + cliente.nome);
+  console.log("Email: " + cliente.email);
+  console.log("Telefone: " + cliente.telefone);
+}
+
+// Executando as funções
+exibirTitulo();
+cadastrarCliente("João Silva", "joao@email.com", "(11) 99999-8888");
+listarCliente();
