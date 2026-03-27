b27ad09e6497d32edf6859c61889dee5a27cd08e
Daniel <daniielgroth@gmail.com>
2026-03-26 21:47:02 -0300

    menu bebidas

---
+++ menu bebidas.js
@@ -0,0 +1,16 @@
+let opcao = 2;
+
+switch (opcao) {
+    case 1:
+        console.log("Coca-cola");
+        break;
+    case 2:
+        console.log("Pepsi");
+        break;
+    case 3:
+        console.log("Fanta");
+        break;
+    default:
+        console.log("Opção inválida!");
+        break;
+    } 
